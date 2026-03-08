b318835645420187ccf4b335f524bdd8935a8ff0
the-Indefatigable <alamkhaan5621@gmail.com>
2026-03-08 13:22:04 -0500

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { lazy, Suspense } from 'react';
 import { ThemeProvider } from './hooks/useTheme';
 import { SettingsProvider } from './hooks/SettingsProvider';
@@ -43,6 +44,7 @@ export default function App() {
           </div>
         </SettingsProvider>
       </BrowserRouter>
+      <Analytics />
     </ThemeProvider>
   );
 }
